28f18f5b4cffa5143a0d84dd12de0962cf4b2e52
acollet001 <36398409+acollet001@users.noreply.github.com>
2018-11-23 00:17:18 +0100

    Add files via upload

---
+++ Ressources/Chatbot.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+
+
+public class Chatbot {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+/*
+        if(str.equals("@hello")){
+          String pattern = "EEEEE dd MMMMM 'et il est' HH'h'mm";
+          SimpleDateFormat simpleDateFormat= new SimpleDateFormat(pattern, new Locale("fr", "FR"));
+          String date=simpleDateFormat.format(new Date());
+          System.out.println("Salut "+args[1]+ "! Nous sommes "+ date);
+        }
+        if(str.equals("++")){
+          System.exit(1);
+        }
+      }
+	  }
+}
+*/
---
+++ Ressources/User.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+
+
+public class User {
+  String name;
+
+  public void User(String name) {
+    this.name=name;
+  }
+
+  public String getname() {
+    return this.name;
+  }
+
+}
